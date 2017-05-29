a2f1f5a77570159654856b9209b6c84abb9cc690
Bartlomiej Kowalczyk <bart.kowalczyk@gmail.com>
2017-05-29 19:43:53 +0200

    simplified code + Math.sign() method

--- 7kyu/No ifs no buts/app.js
+++ 7kyu/No ifs no buts/app.js
@@ -1,12 +1,17 @@
 var noIfsNoButs = function (a,b) {
     switch (true) {
-        case a > b:
-            return `${a} is greater than ${b}`;
-            break;
-        case a < b:
-            return `${a} is smaller than ${b}`;
-            break;
-        default:
-            return `${a} is equal to ${b}`;
+        case a > b: return `${a} is greater than ${b}`;
+        case a < b: return `${a} is smaller than ${b}`;break;
+        default: return `${a} is equal to ${b}`;
+    }
+}
+
+//////// Math.sign() ////////
+
+var noIfsNoButs = function (a,b) {
+    switch (Math.sign(a - b)) {
+        case 1: return `${a} is greater than ${b}`;
+        case -1: return `${a} is smaller than ${b}`;
+        case 0: return `${a} is equal to ${b}`;
     }
 }
